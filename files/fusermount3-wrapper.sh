561b66fcad1f55ca186d3d0a7747d3f3505ecf4c
Iliyas Jorio <iliyas@jor.io>
2026-01-12 21:09:17 +0100

    Add fuse 3.18.1

---
+++ files/fusermount3-wrapper.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -z "$_FUSE_COMMFD" ]; then
+    FD_ARGS=
+else
+    FD_ARGS="--env=_FUSE_COMMFD=${_FUSE_COMMFD} --forward-fd=${_FUSE_COMMFD}"
+fi
+
+exec flatpak-spawn --host --watch-bus $FD_ARGS fusermount3 "$@"
